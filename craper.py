dd583f735e377b349bd65e718d0d2bb3ed199df3
Zaning <61484146+zaning424@users.noreply.github.com>
2020-05-17 05:26:17 -0400

    add comment

--- craper.py
+++ craper.py
@@ -13,8 +13,13 @@ class web():
         driver = webdriver.Chrome("C:/Users/jiani/PycharmProjects/FInal/chromedriver.exe")
         url = 'https://finance.yahoo.com/quote/%s/community'% self.code
         driver.get(url)
+        # search "Show More" button on the page, wait until find it and click it
         WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, '//*[@id="canvass-0-CanvassApplet"]/div/button/span'))).click()
+        # wait 4 sec after to load next 20 comments after press button
+        # can decrease to 2 with good internet, increase to 10 with bad internet connection
+        # YahooFinance default only load 20 comments, press showmore button can load 20 More
+        # this proess repeat 4 times to get 100 comments, can add more times to fetch more commetns
         time.sleep(4)
         WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, '//*[@id="canvass-0-CanvassApplet"]/div/button/span'))).click()
@@ -25,8 +30,8 @@ class web():
         WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, '//*[@id="canvass-0-CanvassApplet"]/div/button/span'))).click()
         time.sleep(4)
+        # search the text of comments by xpath
         comments = driver.find_elements_by_xpath(
             "//div[@class='C($c-fuji-grey-l) Mb(2px) Fz(14px) Lh(20px) Pend(8px)']")
         comments1 = [x.text for x in comments]
         return comments1
-
